f646f573261b8472c16f6786db1793e3c4ce8935
raymond92928 <xis767@yahoo.com.hk>
2015-04-21 17:42:07 +0800

    i can't writ the merge sort QQ

---
+++ selection.cpp
@@ -0,0 +1,44 @@
+#include "stdio.h"
+#include "time.h"
+#include <stdlib.h>
+
+void select(int a[1000],int num);
+int main(){
+	srand(time(NULL));
+	int a[1000]={-1},num;
+	printf("input a number between 2~1000 :");
+	scanf("%d",&num);
+	while (num>999 || num <2){
+		printf("input again :");
+		scanf("%d",&num);
+	}
+	for (int i = 0; i < num; i++){
+		a[i]=rand()%10000;  //make 0-99 random number
+	}
+	printf("排序前: ");
+	for (int i = 0; i < num; i++){
+		printf("%d\t",a[i]); 
+	}
+	printf("\n");
+	select(a,num);
+	printf("排序後: ");
+	for (int i = 0; i < num; i++){
+		printf("%d\t",a[i]); 
+	}
+}
+
+void select(int a[1000],int num){
+	int min,lo,c;
+	for(int j = 0 ;j<num;j++){
+		lo=j;min=a[j];
+		for (int i=j+1;i<num;i++){
+			a[i]<min?min=a[i],lo=i:0;
+		}
+		if(lo!=j){			
+			c=a[j];
+			a[j]=a[lo];
+			a[lo]=c;
+		}
+	}
+	
+}
